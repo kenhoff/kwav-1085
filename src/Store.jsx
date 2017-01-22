517140a062c3873b774b66b8f9d83c1bf4cc26a4
Ken Hoff <thekenhoff@gmail.com>
2017-01-22 11:33:41 -0700

    la musica, more layout

--- src/Store.jsx
+++ src/Store.jsx
@@ -4,19 +4,22 @@ import CardList from "./CardList.jsx";
 class Store extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>The record store</h1>
-				<p>Here, you can exchange 3 tracks for 1 higher rarity track.</p>
-				<h2>Your cards</h2>
-				<CardList cards={this.props.playerWorkingLibrary} actionWhenClicked={this.props.addCardFromWorkingLibraryToStore}></CardList>
-				<h2>the store:</h2>
-				<CardList cards={this.props.store} actionWhenClicked={this.props.putCardBackFromStoreToWorkingLibrary}></CardList>
-				<button onClick={this.props.transmuteStoreCards}>transmute!</button>
+			<div className="record-store">
+				<div>
+					<h1>The record store</h1>
+					<div>
+						<h2>Your cards</h2>
+					</div>
+					<CardList cards={this.props.playerWorkingLibrary} actionWhenClicked={this.props.addCardFromWorkingLibraryToStore}></CardList>
+				</div>
+				<div>
+					<p>Here, you can exchange 3 tracks for 1 higher rarity track.</p>
+					<CardList cards={this.props.store} actionWhenClicked={this.props.putCardBackFromStoreToWorkingLibrary}></CardList>
+					<button onClick={this.props.transmuteStoreCards}>Exchange</button>
+				</div>
 			</div>
 		)
 	}
 }
-
-
 
 export default Store;
